0cb228e7139ae0a19cd70a85146669e5d4e51792
Yurii-Hnatenko <gnatenkourij573@gmail.com>
2024-01-17 18:56:23 +0200

    hw-2-v.1.1

---
+++ js/task-2.js
@@ -0,0 +1,11 @@
+function formatMessage(message, maxLength) {
+     let messageFinal = (maxLength >= message.length)? message :  message.substring(0, maxLength) + "...";
+    return messageFinal;
+}
+
+console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
+console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
+console.log(formatMessage("Vestibulum facilisis purus nec", 20)); // "Vestibulum facilisis..."
+console.log(formatMessage("Vestibulum facilisis purus nec", 30)); // "Vestibulum facilisis purus nec"
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 15)); // "Nunc sed turpis..."
+console.log(formatMessage("Nunc sed turpis a felis in nunc fringilla", 41)); // "Nunc sed turpis a felis in nunc fringilla"
